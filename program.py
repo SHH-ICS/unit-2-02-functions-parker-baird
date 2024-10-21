28c8e31fec5a57cfbb11fc0e6f71a503c669f357
parker-baird <parker.baird-ashmore@stu.ocsb.ca>
2024-10-21 14:00:13 +0000

    h

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
+def myFunction("Hello World!"):
   # My Function
+print (myFunction)
